1820a0d56d94a03511ba6dbcd23b19ab4cdfd453
hudbuz <hzb2@cornell.edu>
2016-07-12 17:45:27 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,21 @@
 'use strict';
+
+
+function favIceCream(ice_cream) {
+
+
+
+  return "I love " + ice_cream;
+}
+
+function shouting(text) {
+  return text.toUpperCase();
+}
+
+function roundDown(number) {
+  return Math.floor(number);
+}
+
+function theTruth() {
+  return true;
+}
